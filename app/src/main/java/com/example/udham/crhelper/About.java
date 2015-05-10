5175655bbbbd84f24d770f5c799146bedb9d02e2
MerXer <gurmeetbagri@gmail.com>
2015-05-10 16:10:24 +0530

    ver1.1.2b Splash and About Added

---
+++ app/src/main/java/com/example/udham/crhelper/About.java
@@ -0,0 +1,19 @@
+package com.example.udham.crhelper;
+
+import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+
+public class About extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+
+}
--- app/src/main/java/com/example/udham/crhelper/MainActivity.java
+++ app/src/main/java/com/example/udham/crhelper/MainActivity.java
@@ -90,5 +90,27 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+
+            Intent intent=new Intent(this,About.class);
+            startActivity(intent);
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
 
 }
